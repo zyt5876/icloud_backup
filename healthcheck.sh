0562f1eb20352d71c97d8c1e849288341cb2c36a
allen chen <zyt5876@gmail.com>
2024-11-16 12:11:50 +0000

    1. 能够运行,

--- healthcheck.sh
+++ healthcheck.sh
@@ -12,8 +12,8 @@ print_to_file "$(date "+%Y-%m-%d %H:%M:%S")"
 if [ -f /app/.icloud_backup_data ]; then
         if ! pgrep -f "python icloud_back.py" > /dev/null
         then
-                print_to_file "will run python"
-                python icloud_back.py&
+                print_to_file "health check:will run python..."
+                #python icloud_back.py&
         else
                 print_to_file "is running!"
         fi
